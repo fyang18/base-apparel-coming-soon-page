b83b3b1968a64b55074c435dc0808f7988423dcf
fyang18 <francis.yang@hotmail.fr>
2020-12-15 00:44:16 +0100

    Add html, css , js

---
+++ app.js
@@ -0,0 +1,19 @@
+function checkMail() {
+	var inputText = document.getElementById("inputText");
+	var mailError = document.getElementById("mailError");
+	var mailOk = document.getElementById("mailOk");
+	var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+	if (inputText.value.length === "") {
+		inputText.style.border = "2px solid hsl(0, 93%, 68%)";
+		mailError.style.display = "block";
+	} else if (inputText.value.match(mailformat)) {
+		mailError.style.display = "none";
+		inputText.style.border = "2px solid hsl(0, 36%, 70%)";
+		mailOk.style.display = "block";
+
+	} else {
+		inputText.style.border = "2px solid hsl(0, 93%, 68%)";
+		mailError.style.display = "block";
+	}
+}
